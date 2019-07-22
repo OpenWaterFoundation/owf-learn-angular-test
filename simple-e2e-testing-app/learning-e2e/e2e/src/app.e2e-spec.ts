6adb41a7317848017e2fed939b425e571f277f01
Sofia Gonzalez <sofia.gonzalez@openwaterfoundation.org>
2019-07-22 12:03:09 -0600

    Overcommunicating through code comments for learning purposes

--- simple-e2e-testing-app/learning-e2e/e2e/src/app.e2e-spec.ts
+++ simple-e2e-testing-app/learning-e2e/e2e/src/app.e2e-spec.ts
@@ -1,23 +1,87 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+// The first thing we do is import the page, in this case 'LetsLearnApp'
+// most methods will be written in the page object file 'app.po.ts' 
+import { LetslearnPage } from './app.po';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('letslearn App', () => {
+  // Once we import the page, we declare an object and initialize with an instance of LetsLearnApp
+  let page: LetslearnPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    // This is done in the beforeEach function so that the code can be executed before each test runs
+    page = new LetslearnPage();
   });
 
-  it('should display welcome message', () => {
+  it('Should display Letslearn title', () => {
+    // We navigate to the our application's site
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to learning-e2e!');
+
+    // assertion and matcher toEqual()
+    // expect the title of the page to equal 'Lets Learn E2E'
+    // the getTitle method in 'app.po.ts'
+    expect(page.getTitle()).toEqual('Lets Learn E2E');
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  it('Should start with 1 point', () => {
+    // nagivate to the site
+    // navigateTo() method in 'app.po.ts'
+    page.navigateTo();
+
+    // assertion and matcher toEqual()
+    // expect the points variable to equal 1 upon initial load of site
+    // getPoints() method in 'app.po.ts' file
+    expect(page.getPoints()).toEqual('1');
   });
+
+  it('Should increase points by clicking plus1', () => {
+    // navigate to the site
+    page.navigateTo();
+
+    // assertion and matcher toEqual()
+    // expect the point variable to be 1 upon initial load of the site
+    expect(page.getPoints()).toEqual('1');
+
+    // get the plus1 button and click it
+    // getPlus1Button() method in 'app.po.ts' file
+    page.getPlus1Button().click();
+
+    // assertion and matcher toEqual()
+    // expect the points variable to equal 2
+    expect(page.getPoints()).toEqual('2');
+
+    // get the plus1 button and click once
+    page.getPlus1Button().click();
+    // click twice
+    page.getPlus1Button().click();
+    // click a third time
+    page.getPlus1Button().click();
+
+    // assertion and matcher toEqual()
+    // expect the points variable to equal 5 after 3 additional clicks of the button 
+    expect(page.getPoints()).toEqual('5');
+  });
+
+  it('Should reset points by clicking Reset button', () => {
+    // navigate to the site
+    page.navigateTo();
+
+    // get the plus1 button and click once
+    page.getPlus1Button().click();
+    // click twice
+    page.getPlus1Button().click();
+    // click three times
+    page.getPlus1Button().click();
+
+    // assertion and toEqual() Matcher 
+    // expect the points varialble to equal the value of 4
+    expect(page.getPoints()).toEqual('4');
+  
+    // get the reset button and click it
+    // getResetButton() method in 'app.po.ts' file
+    page.getResetButton().click();
+
+    // assertion and toEqual Matcher
+    // expect the points variable to equal 0 
+    expect(page.getPoints()).toEqual('0');
+  });
+
 });
