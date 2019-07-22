33aa90521ce5e3793783dd573b8251179c0da3ee
Sofia Gonzalez <sofia.gonzalez@openwaterfoundation.org>
2019-07-22 11:12:42 -0600

    Inserting Angular testing examples: unit & e2e

---
+++ simple-e2e-testing-app/learning-e2e/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lets Learn E2E';
+  points = 1;
+
+  plus1() {
+    this.points++;
+  }
+
+  reset() {
+    this.points = 0;
+  }
+}
